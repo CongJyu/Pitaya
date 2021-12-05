5b8d992f60674005e7fedfea1bae4f56027ace79
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2021-12-05 19:59:37 +0800

    Updated HW7

--- HNU-CG/HW7-Q01/HW7-Q01/main.cpp
+++ HNU-CG/HW7-Q01/HW7-Q01/main.cpp
@@ -8,20 +8,55 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <algorithm>
 using namespace std;
-struct Wujiang {
+class Wujiang {
+public:
     string name;
     int body;
-    int power;
     int iq;
+    int power;
 };
+
+bool cmp1(Wujiang a, Wujiang b) {
+    return a.body > b.body;
+}
+bool cmp2(Wujiang a, Wujiang b) {
+    return a.iq > b.iq;
+}
+bool cmp3(Wujiang a, Wujiang b) {
+    return a.power > b.power;
+}
+
 int main() {
-    Wujiang gen;
+    Wujiang gen[100];
+    int i = 0;
     string tmp;
-    cin >> tmp;
-    if (tmp == "#") {
-        return 0;
-    } else {
-        cin >> gen.body >> gen.power >> gen.iq;
+    while (1) {
+        cin >> tmp;
+        if (tmp == "#") {
+            break;
+        } else {
+            gen[i].name = tmp;
+            cin >> gen[i].body >> gen[i].power >> gen[i].iq;
+        }
+        i++;
     }
+    int n = 0;
+    cin >> n;
+    switch (n) {
+        case 1: sort(gen, gen + i, cmp1);
+            break;
+        case 2: sort(gen, gen + i, cmp2);
+            break;
+        case 3: sort(gen, gen + i, cmp3);
+            break;
+    }
+    for (int j = 0; j < i; j++) {
+        cout << gen[j].name << " "
+        << gen[j].body << " "
+        << gen[j].power << " "
+        << gen[j].iq << endl;
+    }
+    return 0;
 }
