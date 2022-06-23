392b549d03ff9fab0ef8f3769a83036c3c34555b
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-06-23 22:32:24 +0800

    update

--- HNU-SummerTerm/T01P14/main.cpp
+++ HNU-SummerTerm/T01P14/main.cpp
@@ -2,47 +2,47 @@
 
 #include <iostream>
 #include <string>
-#include <cmath>
 
 using namespace std;
 
-string day[7] = {"monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"};
-
-int get_day(const string &in) {
-    for (int i = 0; i < 7; ++i) {
-        if (day[i] == in) {
-            return i + 1;
-        }
-    }
-    return -1;
-}
-
 int main() {
-    int t = 0;
-    cin >> t;
-    while (t--) {
+    int n = 0;
+    cin >> n;
+    string day[7] = {
+            "monday",
+            "tuesday",
+            "wednesday",
+            "thursday",
+            "friday",
+            "saturday",
+            "sunday"
+    };
+    for (int i = 0; i < n; ++i) {
         string s, e;
-        int l = 0, r = 0;
-        cin >> s >> e >> l >> r;
-        int d_start = get_day(s);
-        int d_end = get_day(e);
-        int tmp = abs(d_start - d_end) + 1;
-        if (d_start > d_end) {
-            tmp += 7;
-        }
-        int sum = 0, ans;
-        for (int i = l; i <= r; ++i) {
-            if (i % 7 == tmp % 7) {
-                sum++;
-                ans = i;
+        cin >> s >> e;
+        int l, r, ss = 0, ee = 0, x, y;
+        cin >> l >> r;
+        for (int j = 0; j < 7; ++j) {
+            if (s == day[j]) {
+                ss = j;
+            }
+            if (e == day[j]) {
+                ee = j;
             }
         }
-        if (sum == 0) {
+        if (ss > ee) {
+            ss += 7;
+        }
+        x = ee - ss + 1;
+        y = x + 7;
+        if (x >= l && x <= r) {
+            if (y <= r) {
+                cout << "many" << endl;
+            } else {
+                cout << x << endl;
+            }
+        } else {
             cout << "impossible" << endl;
-        } else if (sum == 1) {
-            cout << ans << endl;
-        } else if (sum > 1) {
-            cout << "many" << endl;
         }
     }
     return 0;
