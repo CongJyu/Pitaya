675ebcbbb170a0f83468969bb30eb6cfe7457e25
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-06-23 23:55:14 +0800

    new code

--- HNU-SummerTerm/T02P12/main.cpp
+++ HNU-SummerTerm/T02P12/main.cpp
@@ -3,27 +3,88 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
-int main() {
+bool similar(string long_str, string short_str) {
+    int long_in = (int) long_str.length();
+    int short_in = (int) short_str.length();
     string tmp;
-    vector<string> dic;
-    while (true) {
-        cin >> tmp;
-        if (tmp == "#") {
-            break;
+    for (int i = 0; i < short_in; ++i) {
+        if (long_str[i] != short_str[i]) {
+            tmp = short_str.substr(0, i);
+            tmp += long_str[i] + short_str.substr(i, short_in - i);
+            if (tmp == long_str) {
+                return true;
+            }
         }
-        dic.push_back(tmp);
     }
-    vector<string> req;
-    while (true) {
-        cin >> tmp;
-        if (tmp == "#") {
-            break;
-        }
-        req.push_back(tmp);
+    tmp = short_str + long_str[long_in - 1];
+    if (tmp == long_str) {
+        return true;
+    } else {
+        return false;
     }
+}
 
+int main() {
+    vector<string> dictionary;
+    vector<string>::iterator it;
+    string word;
+    string dic;
+    bool flag;
+    int sub;
+    while (true) {
+        cin >> dic;
+        if (dic == "#") {
+            break;
+        }
+        dictionary.push_back(dic);
+    }
+    while (true) {
+        cin >> word;
+        if (word == "#") {
+            break;
+        }
+        flag = false;
+        for (it = dictionary.begin(); it != dictionary.end(); it++) {
+            dic = *it;
+            if (dic == word) {
+                cout << word << " is correct";
+                flag = true;
+            }
+        }
+        if (!flag) {
+            cout << word << ":";
+            for (it = dictionary.begin(); it != dictionary.end(); it++) {
+                dic = *it;
+                sub = dic.length() - word.length();
+                if (sub == 0) {
+                    for (int i = 0; i < (int) dic.length(); ++i) {
+                        if (dic[i] != word[i]) {
+                            string temp = word;
+                            temp[i] = dic[i];
+                            if (temp == dic) {
+                                cout << " " << dic;
+                            }
+                            break;
+                        }
+                    }
+                } else if (abs(sub) == 1) {
+                    if (dic.length() < word.length()) {
+                        if (similar(word, dic)) {
+                            cout << " " << dic;
+                        }
+                    } else {
+                        if (similar(dic, word)) {
+                            cout << " " << dic;
+                        }
+                    }
+                }
+            }
+        }
+        cout << endl;
+    }
     return 0;
 }
