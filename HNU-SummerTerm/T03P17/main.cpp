dc55e40443b054553304386e821ff0a2d7ffe5ce
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-06-25 16:39:14 +0800

    add t03p18

---
+++ HNU-SummerTerm/T03P17/main.cpp
@@ -0,0 +1,15 @@
+// t03p17
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string input, compare;
+    while (cin >> input >> compare) {
+        // code
+        
+    }
+    return 0;
+}
