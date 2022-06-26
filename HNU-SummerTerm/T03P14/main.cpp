41404800b8b7f0cdf89eb83d511a191364c01592
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-06-26 21:29:58 +0800

    add t03p14

--- HNU-SummerTerm/T03P14/main.cpp
+++ HNU-SummerTerm/T03P14/main.cpp
@@ -16,20 +16,45 @@ int main() {
         if (item == "@END@") {
             break;
         }
+        dictionary.push_back(item);
         cin.get();
         getline(cin, explanation);
-        dictionary.push_back(item);
         exp.push_back(explanation);
     }
     int n = 0;
     cin >> n;
     while (n--) {
         string search;
+        cin.get();
         getline(cin, search);
+        cin.get();
+        bool flag = false;
         if (search[0] == '[') {
             // 遍历魔咒
+            for (int i = 0; i < (int) dictionary.size(); ++i) {
+                if (dictionary.at(i).find(search) != string::npos) {
+                    cout << exp.at(i) << endl;
+                    flag = true;
+                    break;
+                }
+            }
         } else {
             // 遍历解释
+            for (int i = 0; i < (int) dictionary.size(); ++i) {
+                if (exp.at(i).find(search) != string::npos) {
+                    for (int j = 1; j < (int) dictionary.at(i).size() - 1; ++j) {
+                        cout << dictionary.at(i)[j];
+                    }
+                    cout << endl;
+                    flag = true;
+                    break;
+                }
+            }
+        }
+        if (flag) {
+            continue;
+        } else {
+            cout << "what?" << endl;
         }
     }
     return 0;
