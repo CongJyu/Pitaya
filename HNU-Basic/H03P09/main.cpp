ee47bd8998a5055c72de7c9eb892a0ffe0b91a6b
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-07-05 17:28:49 +0800

    add h03p09

---
+++ HNU-Basic/H03P09/main.cpp
@@ -0,0 +1,47 @@
+// h03p09
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    char input = 0;
+    cin >> input;
+    switch (input) {
+        case '0':
+            cout << "Zero" << endl;
+            break;
+        case '1':
+            cout << "One" << endl;
+            break;
+        case '2':
+            cout << "Two" << endl;
+            break;
+        case '3':
+            cout << "Three" << endl;
+            break;
+        case '4':
+            cout << "Four" << endl;
+            break;
+        case '5':
+            cout << "Five" << endl;
+            break;
+        case '6':
+            cout << "Six" << endl;
+            break;
+        case '7':
+            cout << "Seven" << endl;
+            break;
+        case '8':
+            cout << "Eight" << endl;
+            break;
+        case '9':
+            cout << "Nine" << endl;
+            break;
+        default:
+            cout << "Other" << endl;
+            break;
+    }
+    return 0;
+}
