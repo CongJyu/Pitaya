b25a4ac99338de24f32a59db551d1712c3bcfe8c
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-07-10 21:49:44 +0800

    add h03p14

--- HNU-Basic/H03P14/main.cpp
+++ HNU-Basic/H03P14/main.cpp
@@ -7,10 +7,14 @@ using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
-    int a[4] = {0};
-    for (int i = 0; i < 4; ++i) {
-        cin >> a[i];
+    int x[4] = {0};
+    for (int &i: x) {
+        cin >> i;
     }
-    
+    sort(x, x + 4);
+    int a = x[3] - x[2];
+    int b = x[3] - x[1];
+    int c = x[3] - x[0];
+    cout << a << " " << b << " " << c << endl;
     return 0;
 }
