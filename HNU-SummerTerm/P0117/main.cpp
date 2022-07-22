2e54a8a050dc9f11e9608277fc920c8ef8a56e67
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-07-22 17:56:55 +0800

    add ccf-csp-problemset

---
+++ HNU-SummerTerm/P0117/main.cpp
@@ -0,0 +1,19 @@
+// p0117
+
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    int n{};
+    cin >> n;
+    map<string, int> team;
+    for (int i{}; i < n; ++i) {
+        string name1, name2;
+        int num1{}, num2{};
+        
+    }
+    return 0;
+}
