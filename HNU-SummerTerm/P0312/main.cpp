c47e62514cfa7024afaaabd44a6811218c2859d5
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-07-30 17:43:36 +0800

    add p0321

---
+++ HNU-SummerTerm/P0312/main.cpp
@@ -0,0 +1,14 @@
+// p0312
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    int n{};
+    while (n--) {
+        // calendar
+    }
+    return 0;
+}
