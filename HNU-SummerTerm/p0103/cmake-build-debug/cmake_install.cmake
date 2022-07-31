91e0811648610e0dd9a7161c56884491b1c45027
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-07-31 16:59:57 +0800

    rename

--- HNU-SummerTerm/p0103/cmake-build-debug/cmake_install.cmake
+++ HNU-SummerTerm/p0103/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/rainchen/CodingArea/tmp-cg/p0103
+# Install script for directory: /Users/rainchen/CodingArea/Pitaya/HNU-SummerTerm/P0103
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/rainchen/CodingArea/tmp-cg/p0103/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/rainchen/CodingArea/Pitaya/HNU-SummerTerm/P0103/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
