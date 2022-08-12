1e8cf10599eae067a76170a727a6f3327ffe883d
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-08-12 23:51:35 +0800

    add ccf-csp 201403-3
    有一部分没写完

---
+++ ccf-csp-problemset/201403-3/main.cpp
@@ -0,0 +1,95 @@
+// ccf-csp 201403-3
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Option {
+    char cmd;
+    int append{};
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    string s;
+    cin >> s;
+    vector<Option> opt;
+    for (int i{}; i < (int) s.length(); ++i) {
+        if (i != (int) s.length() - 1 and s[i + 1] != ':') {
+            // 无参数
+            Option tmp{s[i], 0};
+            opt.push_back(tmp);
+        } else if (i == (int) s.length() - 1 and s[i] != ':') {
+            // 无参数
+            Option tmp{s[i], 0};
+            opt.push_back(tmp);
+        } else {
+            // 有参数
+            Option tmp{s[i], 1};
+            opt.push_back(tmp);
+        }
+    }
+    int n{};
+    cin >> n;
+    int case_num{};
+    cin.get();
+    while (n--) {
+        // 预处理
+        vector<string> out;
+        // 输入处理
+        case_num++;
+        string input;
+        getline(cin, input);
+        for (int i{2}; i < (int) input.length(); ++i) {
+            if (input[i] == '-') {
+                // 处理命令
+                bool found_command{false};
+                for (int j{}; j < (int) opt.size(); ++j) {
+                    if (opt.at(j).cmd == input[i + 1]) {
+                        found_command = true;
+                        if (opt.at(j).append == 1) {
+                            // 有参数
+                            int k{i + 2};
+                            while (input[k] == ' ') {
+                                // 没写完
+                            }
+                        } else {
+                            // 无参数，此部分已完成
+                            if (!out.empty()) {
+                                // 检查是否有相同的命令
+                                bool have_same{false};
+                                for (int it{}; it < (int) out.size(); ++it) {
+                                    if (out.at(it).find(input.substr(i, 2)) != string::npos) {
+                                        clog << "==FIND==" << endl;
+                                        clog << out.at(it) << endl;
+                                        have_same = true;
+                                        break;
+                                    }
+                                }
+                                if (!have_same) {
+                                    out.push_back(input.substr(i, 2));
+                                    // 输出动态数组
+                                    clog << "==VECTOR OUT==" << endl;
+                                    for (int test{}; test < (int) out.size(); ++test) {
+                                        clog << out.at(test) << endl;
+                                    }
+                                }
+                            } else {
+                                out.push_back(input.substr(i, 2));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        // 输出处理
+        cout << "Case " << case_num << ": ";
+        for (int i{}; i < (int) out.size(); ++i) {
+            cout << out.at(i) << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
