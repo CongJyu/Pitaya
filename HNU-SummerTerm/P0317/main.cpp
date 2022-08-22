d3a3dc70996f32e5cc22401b0216a01772c5a91d
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-08-22 10:10:44 +0800

    add p0501

---
+++ HNU-SummerTerm/P0317/main.cpp
@@ -0,0 +1,14 @@
+// p0317
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    string rep, cp;
+    cin >> rep >> cp;
+    
+    return 0;
+}
