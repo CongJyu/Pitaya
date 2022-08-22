8a0a613651407ffcdccc0a4ae980719714153257
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-08-22 16:31:40 +0800

    add 201709-1

--- ccf-csp-problemset/201709-1/main.cpp
+++ ccf-csp-problemset/201709-1/main.cpp
@@ -8,6 +8,15 @@ int main() {
     ios::sync_with_stdio(false);
     int n{};
     cin >> n;
-    // 未完待续
+    int current{n / 10}, total{n / 10};
+    while (current >= 5) {
+        current -= 5;
+        total += 2;
+    }
+    while (current >= 3 and current < 5) {
+        current -= 3;
+        total++;
+    }
+    cout << total << endl;
     return 0;
 }
