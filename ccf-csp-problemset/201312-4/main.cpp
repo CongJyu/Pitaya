9ea4bc11705be56440b6fe7b4d7b9c8a77dc2cd4
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-08-22 16:20:14 +0800

    add ccf-csp 201312-4
    不完全对

--- ccf-csp-problemset/201312-4/main.cpp
+++ ccf-csp-problemset/201312-4/main.cpp
@@ -1,37 +1,31 @@
 // ccf-csp 201312-4
-// no idea
 
 #include <iostream>
 
 using namespace std;
 
-const long long mod{1000000007};
-
-void solution(int n) {
-    long long **s{new long long *[n]};
-    for (long long i{}; i < n; ++i) {
-        s[i] = new long long[6];
-    }
-    for (int i{}; i < 6; ++i) {
-        s[0][i] = 0;
-    }
-    for (long long i{1}; i <= n; ++i) {
-        long long j{i - 1};
-        s[i][0] = 1;
-        s[i][1] = (s[j][0] + s[j][1] * 2) % mod;
-        s[i][2] = (s[j][0] + s[j][2]) % mod;
-        s[i][3] = (s[j][1] + s[j][3] * 2) % mod;
-        s[i][4] = (s[j][1] + s[j][2] + s[j][4] * 2) % mod;
-        s[i][5] = (s[j][3] + s[j][4] + s[j][5] * 2) % mod;
-    }
-    cout << s[n][5] << endl;
-    delete[] s;
-}
+const int mod{1000000007};
+int c[1010][1010];
 
 int main() {
     ios::sync_with_stdio(false);
     int n{};
     cin >> n;
-    solution(n);
+
+    for (int i{}; i <= n; ++i) {
+        for (int j{}; j <= i; ++j) {
+            if (!j) {
+                c[i][j] = 1;
+            } else {
+                c[i][j] = (c[i - 1][j] + c[i - 1][j - 1]) % mod;
+            }
+        }
+    }
+
+    long long result{};
+    for (int i{2}; i <= n - 2; ++i) {
+        result = (result + (long long) (c[n - 1][i] * (i - 1) * (n - i - 1))) % mod;
+    }
+    cout << result << endl;
     return 0;
 }
