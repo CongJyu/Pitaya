58cd18a1fe98ca8936e2a43bfaed12a11c2dc654
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-08-22 08:04:32 +0800

    add p0108

--- HNU-SummerTerm/P0108/main.cpp
+++ HNU-SummerTerm/P0108/main.cpp
@@ -1,5 +1,4 @@
 // p0108
-// runtime error
 
 #include <iostream>
 #include <string>
@@ -8,17 +7,24 @@ using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
-    string s_num;
-    cin >> s_num;
-    int n{(int) s_num.length()};
-    long long num = stoll(s_num);
-    s_num += s_num;
+    string num, judge;
+    cin >> num;
+    judge = num + num;
+    int length{(int) num.length()};
     bool flag{true};
-    for (long long i{1}; i <= n; ++i) {
-        long long test = num * i;
-        string s_test = to_string(test);
-        if (s_num.find(s_test) == string::npos) {
+    string com{num};
+    string::size_type found;
+    for (int i{2}; i <= length; ++i) {
+        int temp{};
+        for (int j = length - 1; j >= 0; --j) {
+            temp = (num[j] - '0') * i + temp;
+            com[j] = (char) (temp % 10 + '0');
+            temp /= 10;
+        }
+        found = judge.find(com);
+        if (found == string::npos) {
             flag = false;
+            break;
         }
     }
     if (flag) {
