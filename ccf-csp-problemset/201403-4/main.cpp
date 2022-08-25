c1ee315f4a24971964b1d018e16a1170afb3bc2c
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-08-25 16:23:05 +0800

    add ccf-csp 201403-4

--- ccf-csp-problemset/201403-4/main.cpp
+++ ccf-csp-problemset/201403-4/main.cpp
@@ -1,22 +1,58 @@
 // ccf-csp 201403-4
 
+/*
+ * 有条件的 bfs 遍历
+ * 走有放置路由器的点
+ * 半径在 r 之内
+ */
+
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
-struct Router {
-    int x;
-    int y;
+const int N{20000};
+
+struct Node {
+    long long x, y;  // 坐标
+    int cross;
 };
+
+Node mp[N];
+queue<Node> q;
+long long n, m, k, r;
+bool visited[N];
+
+int bfs() {
+    visited[1] = true;
+    q.push({mp[1].x, mp[1].y, 0});
+    while (!q.empty()) {
+        Node current{q.front()};
+        q.pop();
+        if (current.x == mp[2].x and current.y == mp[2].y) {
+            return current.cross - 1;
+        }
+        for (int i{1}; i <= n + m; ++i) {
+            if (visited[i]) {
+                continue;
+            }
+            if ((mp[i].x - current.x) * (mp[i].x - current.x) + (mp[i].y - current.y) * (mp[i].y - current.y) > r * r) {
+                continue;
+            }
+            visited[i] = true;
+            q.push({mp[i].x, mp[i].y, current.cross + 1});
+        }
+    }
+    return -1;
+}
 
 int main() {
     ios::sync_with_stdio(false);
-    int n{}, m{}, k{}, r{};
     cin >> n >> m >> k >> r;
-    Router *rt{new Router[n]};
-    for (int i{}; i < n; ++i) {
-        cin >> rt[i].x >> rt[i].y;
+    for (int i{1}; i <= n + m; ++i) {
+        cin >> mp[i].x >> mp[i].y;
     }
-    // no idea
+    int ans{bfs()};
+    cout << ans << endl;
     return 0;
 }
