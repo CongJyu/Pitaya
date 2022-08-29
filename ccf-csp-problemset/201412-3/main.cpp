846ba44d0679a3795c5d13c4788ea23a1acf292d
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-08-29 15:09:47 +0800

    add ccf-csp 201412-3

--- ccf-csp-problemset/201412-3/main.cpp
+++ ccf-csp-problemset/201412-3/main.cpp
@@ -1,28 +1,13 @@
 // ccf-csp 201412-3
 
 #include <iostream>
-#include <iomanip>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-struct Record {
-    string opt;
-    double p{};
-    double s{};
-};
-
 int main() {
     ios::sync_with_stdio(false);
-    string order;
-    while (cin >> order) {
-        if (order == "buy") {
-            // 购买股票
-        } else if (order == "sell") {
-            // 出售股票
-        } else if (order == "cancel") {
-            // 撤销记录
-        }
-    }
+
     return 0;
 }
