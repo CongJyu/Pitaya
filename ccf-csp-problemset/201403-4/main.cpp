eb3e8316cff54c2bd3e5f3408856c5c8244bb70b
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-08-31 17:53:07 +0800

    add ccf-csp 201403-4

--- ccf-csp-problemset/201403-4/main.cpp
+++ ccf-csp-problemset/201403-4/main.cpp
@@ -1,58 +1,86 @@
 // ccf-csp 201403-4
 
-/*
- * 有条件的 bfs 遍历
- * 走有放置路由器的点
- * 半径在 r 之内
- */
-
 #include <iostream>
+#include <cstring>
+#include <cmath>
 #include <queue>
 
 using namespace std;
 
-const int N{20000};
+const int maxm{100}, maxn{100};
+int n{}, m{}, k{};
+double r{};
 
-struct Node {
-    long long x, y;  // 坐标
-    int cross;
+struct T_Node {
+    double x, y;
+    int type;
 };
 
-Node mp[N];
-queue<Node> q;
-long long n, m, k, r;
-bool visited[N];
+T_Node node[maxn + maxm + 10];
 
-int bfs() {
-    visited[1] = true;
-    q.push({mp[1].x, mp[1].y, 0});
-    while (!q.empty()) {
-        Node current{q.front()};
-        q.pop();
-        if (current.x == mp[2].x and current.y == mp[2].y) {
-            return current.cross - 1;
-        }
-        for (int i{1}; i <= n + m; ++i) {
-            if (visited[i]) {
-                continue;
-            }
-            if ((mp[i].x - current.x) * (mp[i].x - current.x) + (mp[i].y - current.y) * (mp[i].y - current.y) > r * r) {
-                continue;
-            }
-            visited[i] = true;
-            q.push({mp[i].x, mp[i].y, current.cross + 1});
-        }
-    }
-    return -1;
+int to[(maxn + maxm) * (maxn + maxm) * 2 + 10];
+int nex[(maxn + maxm) * (maxn + maxm) * 2 + 10];
+int head[maxn + maxm + 10], cnt = 0;
+
+void add_edge(int x, int y) {
+    to[cnt] = y;
+    nex[cnt] = head[x];
+    head[x] = cnt++;
+    to[cnt] = x;
+    nex[cnt] = head[y];
+    head[y] = cnt++;
 }
+
+int vis[maxn + maxm + 10][maxm + 10];
+
+struct T_NNode {
+    int a, b, dis;
+
+    T_NNode(int aa, int bb, int ddis) : a(aa), b(bb), dis(ddis) {}
+};
 
 int main() {
     ios::sync_with_stdio(false);
     cin >> n >> m >> k >> r;
     for (int i{1}; i <= n + m; ++i) {
-        cin >> mp[i].x >> mp[i].y;
+        cin >> node[i].x >> node[i].y;
+        node[i].type = (i <= n) ? 0 : 1;
     }
-    int ans{bfs()};
-    cout << ans << endl;
+    memset(head, -1, sizeof(head));
+    for (int i{1}; i <= n + m; ++i) {
+        for (int j{i + 1}; j <= n + m; ++j) {
+            double dist{sqrt(pow((node[i].x - node[j].x), 2.0) + pow((node[i].y - node[j].y), 2.0))};
+            if (dist <= r) {
+                add_edge(i, j);
+            }
+        }
+    }
+    memset(vis, 0, sizeof(vis));
+    queue<T_NNode> q;
+    q.push(T_NNode(1, 0, 0));
+    vis[1][0] = 1;
+    while (true) {
+        T_NNode x{q.front()};
+        q.pop();
+        int a{x.a}, b{x.b}, dis{x.dis};
+        if (a == 2) {
+            cout << dis - 1 << endl;
+            break;
+        }
+        for (int i{head[a]}; i != -1; i = nex[i]) {
+            int l{to[i]};
+            if (node[l].type == 0) {
+                if (!vis[l][b]) {
+                    q.push(T_NNode(l, b, dis + 1));
+                    vis[l][b] = 1;
+                }
+            } else {
+                if (b < k and !vis[l][b + 1]) {
+                    q.push(T_NNode(l, b + 1, dis + 1));
+                    vis[l][b + 1] = 1;
+                }
+            }
+        }
+    }
     return 0;
 }
