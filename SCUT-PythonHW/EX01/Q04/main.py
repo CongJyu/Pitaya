eca6615c4a3c94d486e635c907164fa3ebb7cfc7
Rain Chen <rainchen@Rains-MacBook-Pro.local>
2022-09-03 22:54:01 +0800

    order

---
+++ SCUT-PythonHW/EX01/Q04/main.py
@@ -0,0 +1,11 @@
+# encoding utf-8
+# Q04
+
+import turtle
+
+turtle.setup(500, 500, 50, 50)
+for i in range(12 * 4):
+    for j in range(4):
+        turtle.forward(25)
+        turtle.right(90)
+    turtle.left(15)
