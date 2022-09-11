629bffd28f960568f8938415c366b9484f1dbdec
Chen-Rain <87171700+Chen-Rain@users.noreply.github.com>
2022-09-11 10:53:32 +0800

    add ccf-csp 201512-3

--- ccf-csp-problemset/201512-3/main.cpp
+++ ccf-csp-problemset/201512-3/main.cpp
@@ -1,68 +1,75 @@
 // ccf-csp 201512-3
 
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
-char draw[101][101]{'.'};
+struct Point {
+    int x, y;
+
+    Point(int xx, int yy) : x(xx), y(yy) {}
+};
+
+char pic[100][100]{0};
+int dx[4]{0, 1, 0, -1};
+int dy[4]{1, 0, -1, 0};
 
 int main() {
     ios::sync_with_stdio(false);
-    int n{}, m{}, q{};
-    cin >> n >> m >> q;
-    while (q--) {
-        int opt{};
-        cin >> opt;
-        if (opt == 0) {
-            // 画线段啦
-            int x1{}, y1{}, x2{}, y2{};
-            cin >> x1 >> y1 >> x2 >> y2;
-            if (y1 == y2) {
-                // 横线
-                for (int i{x1}; i <= x2; ++i) {
-                    if (draw[i][y1] == '|') {
-                        draw[i][y1] = '+';
-                    } else {
-                        draw[i][y1] = '-';
-                    }
-                }
-            } else if (x1 == x2) {
-                // 竖线
-                for (int j{y1}; j <= y2; ++j) {
-                    if (draw[x1][j] == '-') {
-                        draw[x1][j] = '+';
-                    } else {
-                        draw[x1][j] = '|';
-                    }
-                }
-            }
-        } else {
-            // 填充啦
-            int x{}, y{};
-            cin >> x >> y;
-            char c{};
-            cin >> c;
-            int i{x}, j{y};
-            while (true) {
-                if (draw[i][j] == '|') {
-                    i++;
-                } else if (draw[i][j] == '-') {
-                    j++;
-                } else {
-                    draw[i][j] = c;
-                }
-                if (i == n and j == m) {
-                    break;
-                }
-            }
+    int m{}, n{}, q{}, op{}, x1{}, x2{}, y1{}, y2{};
+    char ch;
+    cin >> m >> n >> q;
+    for (int i{}; i < m; ++i) {
+        for (int j{}; j < n; ++j) {
+            pic[i][j] = '.';
         }
     }
-    // 输出
-    for (int i{n - 1}; i >= 0; ++i) {
-        for (int j{}; j < m; ++j) {
-            cout << draw[i][j];
+    for (int qi{}; qi < q; ++qi) {
+        cin >> op;
+        if (op == 0) {
+            cin >> x1 >> y1 >> x2 >> y2;
+            if (x1 == x2) {
+                if (y1 > y2) {
+                    swap(y1, y2);
+                }
+                for (int i{y1}; i <= y2; ++i) {
+                    if (pic[x1][i] == '-') {
+                        // '-' + '|' = '+'
+                        pic[x1][i] = '+';
+                        continue;
+                    } else if (pic[x1][i] == '|' or pic[x1][i] == '+') {
+                        // 已经存在
+                        continue;
+                    } else {
+                        // 原地图无分隔
+                        pic[x1][i] = '|';
+                    }
+                }
+            } else {
+                // x 轴方向划线
+                if (x1 > x2) {
+                    swap(x1, x2);
+                }
+                for (int i{x1}; i <= x2; ++i) {
+                    if (pic[i][y1] == '|') {
+                        // '|' + '-' = '+'
+                        pic[i][y1] = '+';
+                        continue;
+                    } else if (pic[i][y1] == '-' or pic[i][y1] == '+') {
+                        continue;
+                    } else {
+                        // 原地图无分隔符
+                        pic[i][y1] = '-';
+                    }
+                }
+            }
+        } else if (op == 1) {
+            if (x1 > x2) {
+                swap(x1, x2);
+            }
+            
         }
-        cout << endl;
     }
     return 0;
 }
