fafcd2d9173959fb896d45bd19f9afcaba61b39a
Chen-Rain <87171700+Chen-Rain@users.noreply.github.com>
2022-10-12 00:50:41 +0800

    add E05Q01

---
+++ SCUT-PythonHW/E05Q01/main.py
@@ -0,0 +1,43 @@
+# encoding utf-8
+# E05Q01
+
+import math
+
+
+class Vector2D(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        r = Vector2D
+        r.x = self.x + other.x
+        r.y = self.y + other.y
+        return r
+
+    def __sub__(self, other):
+        r = Vector2D
+        r.x = self.x - other.x
+        r.y = self.y - other.y
+        return r
+
+    def __mul__(self, other):
+        r = Vector2D
+        r.x = self.x * other.x
+        r.y = self.y * other.y
+        return r
+
+    def __abs__(self):
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    # def __str__(self):
+
+
+a = Vector2D(1, 2)
+b = Vector2D(2, 3)
+print(a + b)
+print(a * b)
+print(b - a)
+print(abs(a))
+# print(str(b))
+# print(a.angle(b))
