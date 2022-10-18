7df00cbc03736673d039b8ec72fc9866b61abd54
Chen-Rain <87171700+Chen-Rain@users.noreply.github.com>
2022-10-18 21:07:59 +0800

    Add Final Q03 Not completed

---
+++ SCUT-PythonHW/FinalQ03/main.py
@@ -0,0 +1,11 @@
+# encoding utf-8
+# Final Q03
+
+# /Users/rainchen/Documents/课程学习讲义/大学讲义/华南理工大学/Python程序设计/期末考核作业资料/Q3/Q3.txt
+
+def main():
+    print("Test")
+
+
+if __name__ == "__main__":
+    main()
