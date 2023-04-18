98ad9872e5648e04b9cff73aa8d5a49fba2a3d26
Rain Chen <87171700+CongJyu@users.noreply.github.com>
2023-04-18 17:33:01 +0800

    configure

---
+++ Fortran-Course/Fortran-Example/example2-2/cmake-build-debug/CMakeFiles/example2_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example2_2.dir/main.f90.o"
+  "example2_2"
+  "example2_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang Fortran)
+  include(CMakeFiles/example2_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
